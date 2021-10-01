3c5ce548a8033f410f22bd7e5d8dae86e67ac284
Burhan <Burhankhalid302@gmail.com>
2021-10-01 06:21:42 +0500

    herer is first commit

---
+++ first-java.js
@@ -0,0 +1,10 @@
+// document.getElementById("abc").innerHTML="in the name of Allah";
+// document.getElementsByTagName("h3")[0].innerHTML=11;
+// document.getElementsByTagName("h3")[1].innerHTML="burhan khalid";
+// document.getElementsByTagName("h3")[2].innerHTML=12;
+// document.getElementsByClassName("burhan")[0].innerHTML="sammi khalod";
+// console.log("burhan khalid");
+
+
+
+
